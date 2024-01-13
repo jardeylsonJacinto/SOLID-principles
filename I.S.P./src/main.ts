88a8602693cdd60490ce94253d248a1e6305ea85
jardeylson jacinto <jardeylsong.m@gmail.com>
2024-01-13 15:26:58 -0300

    feat: add customer

--- I.S.P./src/main.ts
+++ I.S.P./src/main.ts
@@ -1,17 +1,32 @@
 import { ShoppingCart } from './classes/shopping-cart';
 import { Order } from './classes/order';
-import { Messaging } from '../src/services/messaging';
-import { Persistency } from '../src/services/persistency';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
-import { FiftyPercentDiscount } from './classes/discount';
+import { NoDiscount } from './classes/discount';
+import { EnterpriseCustomer } from './classes/customer';
 
-const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
 // const tenPercentDiscount = new TenPercentDiscount();
-// const noDiscount = new NoDiscount();
-const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+// const individualCustomer = new IndividualCustomer(
+//   'Luiz',
+//   'Miranda',
+//   '111.111.111-11',
+// );
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Empresa Gigante',
+  '2222222222222222',
+);
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  enterpriseCustomer,
+);
 
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Caderno', 9.9123));
