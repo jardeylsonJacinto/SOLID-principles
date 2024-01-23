9a2dd34b1a1b0724ad7b33f5dad69cc08a19e9d4
jardeylson jacinto <jardeylsong.m@gmail.com>
2024-01-23 08:11:21 -0300

    test: add customer.spec

--- TS-TEST-JEST/src/classes/customer.ts
+++ TS-TEST-JEST/src/classes/customer.ts
@@ -2,46 +2,48 @@ import {
   IndividualCustomerProtocol,
   EnterpriseCustomerProtocol,
   CustomerOrder,
-} from './protocols/customer-protocol';
+} from './protocols/customer-protocol'
 
 export class IndividualCustomer
-  implements IndividualCustomerProtocol, CustomerOrder {
-  firstName: string;
-  lastName: string;
-  cpf: string;
-  cnpj: string;
+  implements IndividualCustomerProtocol, CustomerOrder
+{
+  firstName: string
+  lastName: string
+  cpf: string
+  cnpj: string
 
   constructor(firstName: string, lastName: string, cpf: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.cpf = cpf;
-    this.cnpj = '';
+    this.firstName = firstName
+    this.lastName = lastName
+    this.cpf = cpf
+    this.cnpj = ''
   }
 
   getName(): string {
-    return this.firstName + ' ' + this.lastName;
+    return this.firstName + ' ' + this.lastName
   }
 
   getIDN(): string {
-    return this.cpf;
+    return this.cpf
   }
 }
 
 export class EnterpriseCustomer
-  implements EnterpriseCustomerProtocol, CustomerOrder {
-  name: string;
-  cnpj: string;
+  implements EnterpriseCustomerProtocol, CustomerOrder
+{
+  name: string
+  cnpj: string
 
   constructor(name: string, cnpj: string) {
-    this.name = name;
-    this.cnpj = cnpj;
+    this.name = name
+    this.cnpj = cnpj
   }
 
   getName(): string {
-    return this.name;
+    return this.name
   }
 
   getIDN(): string {
-    return this.cnpj;
+    return this.cnpj
   }
 }
